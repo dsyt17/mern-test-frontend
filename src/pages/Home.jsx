aa92af19a6d70bd80c63d348cb6665bd120d46c4
Your Name <goncharovigor12@gmail.com>
2022-08-04 18:19:52 +0300

    datetime1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
               title={obj.title}
               imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}/${obj.imageUrl}` : ''}
               user={obj.user}
-              createdAt={(obj.createdAt).toUTCString()}
+              createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
               commentsCount={3}
               tags={obj.tags}            
